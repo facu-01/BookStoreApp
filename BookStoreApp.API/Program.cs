859010c68492ee6e25424d5c1d32b762680b5a0a
Facundo Lucero <facundon.lucero@alumnos.frm.utn.edu.ar>
2025-09-14 00:27:23 -0300

    first

---
+++ BookStoreApp.API/Program.cs
@@ -0,0 +1,40 @@
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+// logging
+builder.Host.UseSerilog((ctxt, lc) =>
+    lc.WriteTo.Console().ReadFrom.Configuration(ctxt.Configuration)
+);
+
+builder.Services.AddCors(opt =>
+{
+    opt.AddPolicy("AllowAll",
+        b => b.AllowAnyMethod()
+            .AllowAnyHeader()
+            .AllowAnyOrigin());
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseCors("AllowAll");
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
